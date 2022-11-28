9bd10fb4127a099ec41281b7ab9714cdcb03530f
pc <pc@DESKTOP-V9AEPEE>
2022-11-28 15:15:32 +0900

    test

--- clientexample/src/main/java/clientexample/ClientExampleController.java
+++ clientexample/src/main/java/clientexample/ClientExampleController.java
@@ -21,7 +21,7 @@ public class ClientExampleController {
 	UIData uiData = new UIData();
 
 	public String serviceUrl() {
-	    return "http://localhost:8012";
+	    return "http://api.223.130.168.242.traefik.me";
 	}
 	
     @RequestMapping(value="/emplist", method=RequestMethod.GET)
